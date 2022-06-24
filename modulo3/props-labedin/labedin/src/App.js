f78aaa5147f3be351902a0f522e167ff284029d3
Vanessa Mainara <mainaravanessa@gmail.com>
2022-06-24 19:27:30 -0300

    Exercicio Props

--- modulo3/props-labedin/labedin/src/App.js
+++ modulo3/props-labedin/labedin/src/App.js
@@ -2,6 +2,15 @@ import React from 'react';
 import './App.css';
 import CardGrande from './components/CardGrande/CardGrande';
 import ImagemButton from './components/ImagemButton/ImagemButton';
+import CardPequeno from './components/CardPequeno/CardPequeno';
+import emailLogo from './img/email_icon.png';
+import addressLogo from './img/address_icon.png';
+import profilePic from './img/vanessa.png';
+import labenuLogo from './img/logoLabenu.png';
+import logoFacul from './img/logoUniFatecie.png';
+import logoInsta from './img/insta.png';
+import logoLinkedin from './img/linkedim.png';
+
 
 function App() {
   return (
@@ -9,7 +18,8 @@ function App() {
       <div className="page-section-container">
         <h2>Dados pessoais</h2>
         <CardGrande 
-          imagem="https://uploads-ssl.webflow.com/5d640f4558306be99cf47a0e/5dd57846babb597b77c6bb1d_PerfilFuture4_cor.png" 
+          imagem={profilePic}
+          //imagem="https://uploads-ssl.webflow.com/5d640f4558306be99cf47a0e/5dd57846babb597b77c6bb1d_PerfilFuture4_cor.png" 
           nome="Vanessa Mainara" 
           descricao="Oi, eu sou Vanessa Mainara. Sou aluna Web Full Stack na Labenu."
         />
@@ -20,32 +30,36 @@ function App() {
         />
       </div>
 
+      <CardPequeno imagem={emailLogo} item='Email:' conteudo='mainaravanessa@gmail.com' />
+
+      <CardPequeno imagem={addressLogo} item='Endereço:' conteudo='Rua Labenu, 1000' />
+
       <div className="page-section-container">
-        <h2>Experiências profissionais</h2>
+        <h2>Formação Academicas</h2>
         <CardGrande 
           
-          imagem="https://s3.amazonaws.com/future4.com.br/static/headf4-c492117ca2373dc85ca81bf715b3dc2a.png" 
+          imagem={labenuLogo}
           nome="Labenu" 
-          descricao="Formando desenvolvedores para o mercado de trabalho!" 
+          descricao="Desenvolvedor Web Full Stack" 
         />
         
         <CardGrande 
-          imagem="https://logodownload.org/wp-content/uploads/2019/03/nasa-logo-0-768x768.png" 
-          nome="NASA" 
-          descricao="Apontando defeitos." 
+          imagem={logoFacul}
+          nome="UniFatecie" 
+          descricao="Análise e Desenvolvimento de Sistemas" 
         />
       </div>
 
       <div className="page-section-container">
         <h2>Minhas redes sociais</h2>
         <ImagemButton 
-          imagem="https://d2v9ipibika81v.cloudfront.net/uploads/sites/261/2017/01/facebook-logo-3.png" 
-          texto="Facebook" 
+          imagem={logoLinkedin}
+          texto="LinkedIn" 
         />        
 
         <ImagemButton 
-          imagem="https://logodownload.org/wp-content/uploads/2014/09/twitter-logo-1-1.png" 
-          texto="Twitter" 
+          imagem={logoInsta} 
+          texto="Instagram" 
         />        
       </div>
     </div>
