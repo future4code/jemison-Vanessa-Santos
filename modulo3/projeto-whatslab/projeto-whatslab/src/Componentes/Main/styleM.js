466631ac3f0e48be5b4edb33aa615c2b580b7302
Vanessa Mainara <mainaravanessa@gmail.com>
2022-07-08 22:37:44 -0300

    Projeto WhatsLab

---
+++ modulo3/projeto-whatslab/projeto-whatslab/src/Componentes/Main/styleM.js
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export const Balao = styled.div`
+    border: solid .04vw;
+    border-radius: 1vw;
+    box-shadow: 0.2vw 0.2vw 0.5vw grey;
+    width:fit-content ;
+    align-items: flex-end;
+    margin: 1vw;
+    padding: 0.5vw;
+    background-color: #00FF00;
+    color: black;
+`
+
+export const DivPai = styled.div`
+display: flex;
+align-items: center;
+margin-left: 30vw;
+margin-top: 2vw;
+
+`
+
+export const Section = styled.section`
+border: 0.02vw solid;
+height: 80vh;
+width: 40vw;
+display: flex;
+flex-direction: column;
+justify-content: flex-end;
+background-color: black;
+font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+`
+
+
+export const MainInput = styled.main`
+display: flex;
+margin: 0.5vw;
+`
+
+export const InputMensagem = styled.div`
+margin: 0vw 0.5vw;
+` 
