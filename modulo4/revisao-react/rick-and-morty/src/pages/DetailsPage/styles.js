034c4c2c567c0d889044e04ab66909a796836124
Vanessa Mainara <mainaravanessa@gmail.com>
2022-09-04 22:22:12 -0300

    Final Projeto

---
+++ modulo4/revisao-react/rick-and-morty/src/pages/DetailsPage/styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container= styled.div`
+    display:flex;
+    justify-content: space-evenly;
+    flex-direction: column;
+    width: 50%;
+    height: 80vh;
+    text-align: center;
+    margin: auto;
+
+    bottom{
+        justify-self: flex-end;
+    }
+
+
+
+    `
